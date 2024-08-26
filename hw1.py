3477e931275149300403b4118ce0046d555a0bbb
Jtrep10 <jtrep10@gmail.com>
2024-08-26 19:45:56 +0000

    added factorial python function

---
+++ hw1.py
@@ -0,0 +1,10 @@
+#Function that calculates factorial
+def factorial(n = 3):
+    fact = 1
+    print(fact)
+    for num in range(2, n + 1):
+        fact *= num
+    return fact
+
+x=factorial()
+print(x)
