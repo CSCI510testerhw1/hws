b8004a33afff2e4fddeb6bb7816a6ee2476b6f65
Jtrep10 <jtrep10@gmail.com>
2024-09-03 12:32:16 +0000

    Added hw2 folder + code and began debugging with pylint and autopep8

---
+++ hw2/hw2_debugging.py
@@ -0,0 +1,40 @@
+"""This module imports the random_array() function from rand.py and uses it to
+generate, then sort a random array in ascending order using the 
+merge sort algorithm"""
+import rand
+
+
+def mergeSort(arr):
+    if len(arr) == 1:
+        return arr
+
+    half = len(arr) // 2
+
+    return recombine(mergeSort(arr[:half]), mergeSort(arr[half:]))
+
+
+def recombine(leftArr, rightArr):
+    leftIndex = 0
+    rightIndex = 0
+    mergeArr = [None] * (len(leftArr) + len(rightArr))
+    while leftIndex < len(leftArr) and rightIndex < len(rightArr):
+        if leftArr[leftIndex] < rightArr[rightIndex]:
+            rightIndex += 1
+            mergeArr[leftIndex + rightIndex] = leftArr[leftIndex]
+        else:
+            leftIndex += 1
+            mergeArr[leftIndex + rightIndex] = rightArr[rightIndex]
+
+    for i in range(rightIndex, len(rightArr)):
+        mergeArr[leftIndex + rightIndex] = rightArr[i]
+
+    for i in range(leftIndex, len(leftArr)):
+        mergeArr[leftIndex + rightIndex] = leftArr[i]
+
+    return mergeArr
+
+
+arr = rand.random_array([None] * 20)
+arr_out = mergeSort(arr)
+
+print(arr_out)
---
+++ hw2/rand.py
@@ -0,0 +1,15 @@
+"""This module utilizes the subprocess.run function in order to generate
+random arrays."""
+import subprocess
+
+
+def random_array(arr):
+    """This function accepts an array of size n as a parameter,
+    and uses the subprocess.run function in order to generate random integers between
+    1 and 20 and fill the array with these random values."""
+    shuffled_num = None
+    for i in range(len(arr)):
+        shuffled_num = subprocess.run(
+            ["shuf", "-i1-20", "-n1"], capture_output=True, check=True)
+        arr[i] = int(shuffled_num.stdout)
+    return arr
