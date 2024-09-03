63d247c0c1da2b1540ea08f098b2bc04007cec4e
Jtrep10 <jtrep10@gmail.com>
2024-09-03 16:32:47 +0000

    debugging.py tutorial

--- debugging.py
+++ debugging.py
@@ -1,20 +1,22 @@
-def selectionSort(arr):
-    n = len(arr)
-    for i in range(n):
-        lowestIndex = i
-        for j in range(n):
-            if arr[j] < arr[lowestIndex]:
-                lowestIndex = j
+import pdb
+def selectionSort(arr):  
+   #breakpoint()
+   n = len(arr)  
+   for i in range(n):  
+       lowestIndex = i  
+       for j in range(i+1, n):  
+           if arr[j] < arr[lowestIndex]:  
+               lowestIndex = j
 
-        arr[lowestIndex] = arr[i]
-        arr[i] = arr[lowestIndex]
-        n = n - 1
+       store = arr[lowestIndex]
+       arr[lowestIndex] = arr[i]  
+       arr[i] = store  
+       #n = n - 1
 
-    return arr
+   return arr
 
 arr_in = [5, 3, 2, 1, 8, 10, 11, 9, 23]
 
 arr_out = selectionSort(arr_in)
 
 print(arr_out)
-
