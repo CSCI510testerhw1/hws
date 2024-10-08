e7092ed4d8b82c76742d68a68d9b3b96b8fd6c13
Jtrep10 <jtrep10@gmail.com>
2024-10-08 15:11:11 +0000

    Some progress on task2 part b

--- hw4/task2.sh
+++ hw4/task2.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-grep "CSC510" \dataset1/file* | sort | uniq -c | cut -d: -f1 | grep -E -v "[1-2] " | grep -E -o "file_[0-9]*"
+grep "CSC510" \dataset1/file* | sort | uniq -c | cut -d: -f1 | grep -E -v "[1-2] " | sort | grep -E -o "file_[0-9]*"
+grep "CSC510" \dataset1/file* | sort | uniq -c | cut -d: -f1 | grep -E -v "[1-2] " | sort -r | gawk '{print $1, $2}'
