76fd2127ed94203854b5279bcc5b4762127ecf62
smule <smule@ncsu.edu>
2024-10-09 18:21:53 -0400

    output

--- hw4/task2.sh
+++ hw4/task2.sh
@@ -7,15 +7,15 @@
 grep -rl "sample" dataset1/file* | xargs grep -c "CSC510" | awk -F: '$2 >= 3' | gawk -F: '{cmd="ls -l \"" $1 "\" | cut -d\" \" -f5"; cmd | getline size; close(cmd); print $1 ":" $2 ":" size}' | sort -t: -k2,2nr -k3,3nr | cut -d: -f1 | sed 's/.*\/file_/filtered_/'
 
 #Output
-#file_20
-#file_10
-#file_4
-#file_24
-#file_14
-#file_22
-#file_2
-#file_30
-#file_3
-#file_23
-#file_13
-#file_12
+# filtered_20
+# filtered_10
+# filtered_4
+# filtered_14
+# filtered_24
+# filtered_22
+# filtered_2
+# filtered_30
+# filtered_3
+# filtered_23
+# filtered_12
+# filtered_13
