e2d5026ecf77e5bbb82bab20d82652730e7c90fe
smule <smule@ncsu.edu>
2024-10-09 18:16:09 -0400

    simplified command

--- hw4/task2.sh
+++ hw4/task2.sh
@@ -4,9 +4,7 @@
 #grep "CSC510" \dataset1/file* | sort | uniq -c | cut -d: -f1 | grep -E -v "[1-2] " | sort -r | grep -E -o "dataset1/file_[0-9]*" | xargs ls -l | sort -k5,5nr | gawk '{print $9}'
 #grep "CSC510" \dataset1/file* | sort | uniq -c | cut -d: -f1 | grep -E -v "[1-2] " | grep -E -o "dataset1/file_[0-9]*" | xargs ls -l | sort -k5,5nr | gawk '{print $9}' | xargs grep "CSC510" | cut -d: -f1 | uniq -c | sort -r | grep -E -o "file_[0-9]*" | sed 's/file_/filtered_/'
 
-grep -rl "sample" dataset1/file* | xargs grep -c "CSC510" | awk -F: '$2 >= 3' | cut -d: -f1 | xargs ls -l | gawk '{print $5, $9}' | sort -k1,1nr | gawk '{print $2}' | xargs grep -c "CSC510" | sort -rn -t: -k2 | gawk -F: '{print $1}' | xargs ls -l | sort -rnk5 | gawk '{print $9}' | xargs grep "CSC510" | cut -d: -f1 | uniq -c | sort -rn | gawk '{print $2}' | sed 's/.*\/file_/filtered_/'
-
-
+grep -rl "sample" dataset1/file* | xargs grep -c "CSC510" | awk -F: '$2 >= 3' | gawk -F: '{cmd="ls -l \"" $1 "\" | cut -d\" \" -f5"; cmd | getline size; close(cmd); print $1 ":" $2 ":" size}' | sort -t: -k2,2nr -k3,3nr | cut -d: -f1 | sed 's/.*\/file_/filtered_/'
 
 #Output
 #file_20
