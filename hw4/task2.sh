42a5be1912961876be7ac688e541c8df200cdb8f
smule <smule@ncsu.edu>
2024-10-09 18:36:52 -0400

    adding grep and uniq for task a

--- hw4/task2.sh
+++ hw4/task2.sh
@@ -4,7 +4,9 @@
 #grep "CSC510" \dataset1/file* | sort | uniq -c | cut -d: -f1 | grep -E -v "[1-2] " | sort -r | grep -E -o "dataset1/file_[0-9]*" | xargs ls -l | sort -k5,5nr | gawk '{print $9}'
 #grep "CSC510" \dataset1/file* | sort | uniq -c | cut -d: -f1 | grep -E -v "[1-2] " | grep -E -o "dataset1/file_[0-9]*" | xargs ls -l | sort -k5,5nr | gawk '{print $9}' | xargs grep "CSC510" | cut -d: -f1 | uniq -c | sort -r | grep -E -o "file_[0-9]*" | sed 's/file_/filtered_/'
 
-grep -rl "sample" dataset1/file* | xargs grep -c "CSC510" | awk -F: '$2 >= 3' | gawk -F: '{cmd="ls -l \"" $1 "\" | cut -d\" \" -f5"; cmd | getline size; close(cmd); print $1 ":" $2 ":" size}' | sort -t: -k2,2nr -k3,3nr | cut -d: -f1 | sed 's/.*\/file_/filtered_/'
+#grep -rl "sample" dataset1/file* | xargs grep -c "CSC510" | awk -F: '$2 >= 3' | gawk -F: '{cmd="ls -l \"" $1 "\" | cut -d\" \" -f5"; cmd | getline size; close(cmd); print $1 ":" $2 ":" size}' | sort -t: -k2,2nr -k3,3nr | cut -d: -f1 | sed 's/.*\/file_/filtered_/'
+
+grep -rl "sample" dataset1/file* | xargs grep -c "CSC510" | grep -E ':[3-9]|:[1-9][0-9]+' | gawk -F: '{cmd="ls -l \"" $1 "\" | cut -d\" \" -f5"; cmd | getline size; close(cmd); print $1 ":" $2 ":" size}' | sort -t: -k2,2nr -k3,3nr | cut -d: -f1 | sed 's/.*\/file_/filtered_/'
 
 #Output
 # filtered_20
