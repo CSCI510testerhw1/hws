2dcfd332f28df80d3d21b0148ec2318a476d5000
Anirudh Kaluri <anirudhkaluri@gmail.com>
2024-10-10 17:50:45 +0000

    simplified the command and commented task3.sh

--- hw4/task3.sh
+++ hw4/task3.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-cat titanic.csv | grep  '^[^,]*,[^,]*,2' | grep  -P 'S\r$' | sed 's/female/F/' | sed 's/male/M/' | grep -o  -E  '[MF],[0-9]+' | cut -d, -f2 | gawk '{sum+=$1;count++} END {print sum/count}'
+#read titanic.csv with cat
+#pass it to grep to filter records with class 2
+#use grep again to get the records which embarked at Southampton
+#modify data from female to M
+#modify data from male to M
+#Remove data points for which age is absent using regex of grep and use -o flag to print only the matching pattern literals
+#Use gawk to find the sum and then average of the second column which is the column with ages
+cat titanic.csv | grep  '^[^,]*,[^,]*,2' | grep  -P 'S\r$' | sed 's/female/F/' | sed 's/male/M/' | grep -o  -E  '[MF],[0-9]+' | gawk -F, '{sum+=$2;count++} END {print sum/count}'
