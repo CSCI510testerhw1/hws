7a08dc1ded987687997f0ba6ea46cbf25076eaae
Jtrep10 <jtrep10@gmail.com>
2024-10-10 18:09:02 +0000

    task3 update

--- hw4/task3.sh
+++ hw4/task3.sh
@@ -6,4 +6,4 @@
 #modify data from male to M
 #Remove data points for which age is absent using regex of grep and use -o flag to print only the matching pattern literals
 #Use gawk to find the sum and then average of the second column which is the column with ages
-cat titanic.csv | grep  '^[^,]*,[^,]*,2' | grep  -P 'S\r$' | sed 's/female/F/' | sed 's/male/M/' | grep -o  -E  '[MF],[0-9]+' | gawk -F, '{sum+=$2;count++} END {print sum/count}'
+cat titanic.csv | grep  '^[^,]*,[^,]*,2' | grep  -P 'S\r$' | sed 's/female/F/' | sed 's/male/M/' | grep -o  -E  '[MF],[0-9]+' | gawk -F, '{sum+=$2;count++} END {print "Average Age:",sum/count}'
