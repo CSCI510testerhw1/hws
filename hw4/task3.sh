e9d76b22017d8eabee0dee48cc11f2c63e1ef270
Anirudh Kaluri <anirudhkaluri@gmail.com>
2024-10-10 18:13:50 +0000

    added tee in the pipeline

--- hw4/task3.sh
+++ hw4/task3.sh
@@ -4,6 +4,7 @@
 #use grep again to get the records which embarked at Southampton
 #modify data from female to M
 #modify data from male to M
+#print the data to terminal before we move on to calculate the average down the pipe
 #Remove data points for which age is absent using regex of grep and use -o flag to print only the matching pattern literals
 #Use gawk to find the sum and then average of the second column which is the column with ages
-cat titanic.csv | grep  '^[^,]*,[^,]*,2' | grep  -P 'S\r$' | sed 's/female/F/' | sed 's/male/M/' | grep -o  -E  '[MF],[0-9]+' | gawk -F, '{sum+=$2;count++} END {print "Average Age:",sum/count}'
+cat titanic.csv | grep  '^[^,]*,[^,]*,2' | grep  -P 'S\r$' | sed 's/female/F/' | sed 's/male/M/' | tee /dev/tty | grep -o  -E  '[MF],[0-9]+' | gawk -F, '{sum+=$2;count++} END {print "Average Age:",sum/count}'
