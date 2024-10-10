dde2eda86fa3b739f986c8b8dd7c569281fc5d5f
Anirudh Kaluri <anirudhkaluri@gmail.com>
2024-10-10 16:26:49 +0000

    changed the task3 solution

--- hw4/task3.sh
+++ hw4/task3.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-#first get all the records that embarked in southampton
-#then filter these records with the condition 2nd class
-#Replace Female with F and Male with M in that order. We get the following example:
-#882 0 2 "Markun  Mr. Johann" M 33 0 0 349257 7.8958  S
-#Then use " as delimiter and get the 3rd column after the name. 
-#After delimiting using " , the third column/field will have a set of columns as shown below:
-#F 25 0 1 230433 26  S
-#The second column in that is age
-#Sum the age and calculate the mean
-grep -P 'S\r$' titanic.csv | gawk -F, '$3=2' | sed 's/female/F/' | sed 's/male/M/' | cut -d '"' -f3 | gawk -F ' ' '{sum+= $2;count++;} END {print "MEAN=",sum/count}' 
+
+#grep -P 'S\r$' titanic.csv | gawk -F, '$3=2' | sed 's/female/F/' | sed 's/male/M/' | cut -d '"' -f3 | grep -E "\s[MF]\s[0-9]" | gawk -F ' ' '{sum+= $2;count++;} END {print "MEAN=",sum/count; print "SUM=",sum;print "COUNT=",count}'
+grep  '^[^,]*,[^,]*,2' titanic.csv | grep  -P 'S\r$' | sed 's/female/F/' | sed 's/male/M/'
