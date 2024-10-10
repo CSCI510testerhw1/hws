95a5570138a903d977e31a38900db438d457b5c0
Anirudh Kaluri <anirudhkaluri@gmail.com>
2024-10-10 17:35:28 +0000

    removed $ before the command in task3.sh

--- hw4/task3.sh
+++ hw4/task3.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-$ cat titanic.csv | grep  '^[^,]*,[^,]*,2' | grep  -P 'S\r$' | sed 's/female/F/' | sed 's/male/M/' | grep -o  -E  '[MF],[0-9]+' | cut -d, -f2 | gawk '{sum+=$1;count++} END {print sum/count}'
+cat titanic.csv | grep  '^[^,]*,[^,]*,2' | grep  -P 'S\r$' | sed 's/female/F/' | sed 's/male/M/' | grep -o  -E  '[MF],[0-9]+' | cut -d, -f2 | gawk '{sum+=$1;count++} END {print sum/count}'
