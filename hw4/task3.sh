1eccd78333d4ef71e8393fb0d0e8a6339a88f669
Anirudh Kaluri <anirudhkaluri@gmail.com>
2024-10-10 17:34:08 +0000

    corrected task3.sh command

--- hw4/task3.sh
+++ hw4/task3.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-
-cat titanic.csv | grep  '^[^,]*,[^,]*,2' | grep  -P 'S\r$' | sed 's/female/F/' | sed 's/male/M/' | grep  -E  '^[1-9]*..,[0-9],2,".*",[MF],[0-9]+' | cut -d '"' -f3 | gawk -F, '{sum+= $3;count++;} END {print "MEAN=",sum/count; print "SUM=",sum;print "COUNT=",count}'
+$ cat titanic.csv | grep  '^[^,]*,[^,]*,2' | grep  -P 'S\r$' | sed 's/female/F/' | sed 's/male/M/' | grep -o  -E  '[MF],[0-9]+' | cut -d, -f2 | gawk '{sum+=$1;count++} END {print sum/count}'
