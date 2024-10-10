61cdd4ecb44a1c3c6649afe8e19840203971beb0
Anirudh Kaluri <anirudhkaluri@gmail.com>
2024-10-10 16:37:04 +0000

    Calculated mean with gawk

--- hw4/task3.sh
+++ hw4/task3.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #grep -P 'S\r$' titanic.csv | gawk -F, '$3=2' | sed 's/female/F/' | sed 's/male/M/' | cut -d '"' -f3 | grep -E "\s[MF]\s[0-9]" | gawk -F ' ' '{sum+= $2;count++;} END {print "MEAN=",sum/count; print "SUM=",sum;print "COUNT=",count}'
-grep  '^[^,]*,[^,]*,2' titanic.csv | grep  -P 'S\r$' | sed 's/female/F/' | sed 's/male/M/'
+grep  '^[^,]*,[^,]*,2' titanic.csv | grep  -P 'S\r$' | sed 's/female/F/' | sed 's/male/M/' | cut -d '"' -f3 | grep -E "^,[MF],[0-9]+,*" | gawk -F, '{sum+= $3;count++;} END {print "MEAN=",sum/count; print "SUM=",sum;print "COUNT=",count}'
